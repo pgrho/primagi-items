4b87b2e4b6ec7c5020eced2499f93ad2d97e4070
pgrho <shipwreck@live.jp>
2022-03-13 09:30:55 +0900

    Added subcategory "エレガント"

--- src/Shipwreck.PrimagiItems/Program.cs
+++ src/Shipwreck.PrimagiItems/Program.cs
@@ -8,11 +8,11 @@ class Program
 {
     private const string src = "https://cdnprimagiimg01.blob.core.windows.net/primagi/assets/data/item.json";
 
-    private static readonly string[] genre = new[] { "カジュアル", "スタイリッシュ", "ライブ" };
-    private static readonly string[] category = new[] { "トップス", "ワンピ", "ボトムス", "シューズ", "アクセ" };
-    private static readonly string[] rarity = new[] { "R", "SR", "UR", "PMR" };
+    private static readonly string[] KNOWN_GENRES = new[] { "カジュアル", "スタイリッシュ", "ライブ" };
+    private static readonly string[] KNOWN_CATEGORIES = new[] { "トップス", "ワンピ", "ボトムス", "シューズ", "アクセ" };
+    private static readonly string[] KNOWN_RARITIES = new[] { "R", "SR", "UR", "PMR" };
 
-    private static readonly string?[] brand = new[] {
+    private static readonly string?[] KNOWN_BRANDS = new[] {
     "LOVELY MELODY",
     "VIVID STAR",
     "Radiant Abyss",
@@ -23,28 +23,32 @@ class Program
     "PrismStone",
 };
 
-    private static readonly string[] color = new[] { "ちゃ", "あか", "ピンク", "オレンジ", "きいろ", "みどり", "みずいろ", "あお", "むらさき", "くろ", "しろ", "シルバー", "ゴールド", };
+    private static readonly string[] KNOWN_COLORS = new[] { "ちゃ", "あか", "ピンク", "オレンジ", "きいろ", "みどり", "みずいろ", "あお", "むらさき", "くろ", "しろ", "シルバー", "ゴールド", };
 
-    private static readonly string?[] subcategory = new[] {
+    private static readonly string?[] KNOWN_SUBCATEGORIES = new[] {
         null,
         "フラワー",
         "スター",
         "ハート",
         "ガーリー",
+
         "キュート",
         "チェック",
         "リボン",
         "クール",
         "ゴシック",
+
         "ジュエル",
         "スポーツ",
         "ダンス",
         "ポップ",
         "ミステリアス",
+
         "ラブリー",
         "スイーツ",
         "ベーシック",
         "アニマル",
+        "エレガント",
     };
 
     static async Task Main()
@@ -54,6 +58,7 @@ class Program
 
         var json = await res.Content.ReadAsStringAsync();
         var jd = JArray.Parse(json);
+        var items = jd.ToObject<List<Item>>()!;
 
         var fe = (JObject)jd[0];
 
@@ -65,6 +70,32 @@ class Program
             di.Create();
         }
 
+        static Dictionary<int, string> toDictionary(string?[] array)
+            => array.Select((e, i) => (e, i)).Where(t => t.e != null).ToDictionary(t => t.i + 1, t => t.e!);
+
+        var rarities = toDictionary(KNOWN_RARITIES);
+        var genres = toDictionary(KNOWN_GENRES);
+        var categories = toDictionary(KNOWN_CATEGORIES);
+        var brands = toDictionary(KNOWN_BRANDS);
+        var colors = toDictionary(KNOWN_COLORS);
+        var subcategories = toDictionary(KNOWN_SUBCATEGORIES);
+
+        static string getValue(Dictionary<int, string> dic, int no, string? errorMessage = null)
+        {
+            if (!dic.TryGetValue(no, out var s))
+            {
+                s = "error#" + no;
+                dic[no] = s;
+
+                if (errorMessage != null)
+                {
+                    Console.Error?.WriteLine(errorMessage);
+                }
+            }
+            return s;
+        }
+
+
         using (var sw = new StreamWriter(new FileStream(Path.Combine(di.FullName, "items.tsv"), FileMode.Create), Encoding.UTF8, 4096))
         {
             foreach (var p in props)
@@ -72,32 +103,56 @@ class Program
                 sw.Write(p);
                 sw.Write('\t');
             }
+            sw.Write("RarityName\t");
+            sw.Write("GenreName\t");
+            sw.Write("CategoryName\t");
+            sw.Write("BrandName\t");
+            sw.Write("ColorName\t");
+            sw.Write("SubcategoryName");
             sw.WriteLine();
-            foreach (JObject obj in jd)
+
+            for (var i = 0; i < items.Count; i++)
             {
+                var obj = (JObject)jd[i];
+                var item = items[i];
+
                 foreach (var p in props)
                 {
                     obj.TryGetValue(p, out var jp);
                     sw.Write(jp);
                     sw.Write('\t');
                 }
+
+                string getValueWithError(Dictionary<int, string> dic, int no, string errorKey)
+                    => getValue(dic, no, $"{item.SealId}: {item.CoordinationName} {errorKey}={no}");
+
+                sw.Write(getValueWithError(rarities, item.Rarity, nameof(item.Rarity)));
+                sw.Write('\t');
+                sw.Write(getValueWithError(genres, item.Genre, nameof(item.Genre)));
+                sw.Write('\t');
+                sw.Write(getValueWithError(categories, item.Category, nameof(item.Category)));
+                sw.Write('\t');
+                sw.Write(getValueWithError(brands, item.Brand, nameof(item.Brand)));
+                sw.Write('\t');
+                sw.Write(getValueWithError(colors, item.Color, nameof(item.Color)));
+                sw.Write('\t');
+                sw.Write(getValueWithError(subcategories, item.SubCategory, nameof(item.SubCategory)));
+
                 sw.WriteLine();
             }
         }
-
-        var items = jd.ToObject<List<Item>>()!;
 
         var cols = new (string, Func<Item, object>)[]
         {
             ("ID", e => e.Id),
             ("刻印", e => e.SealId),
-            ("レアリティ", e => rarity.ElementAtOrDefault(e.Rarity - 1) ?? e.Rarity.ToString()),
+            ("レアリティ", e => getValue(rarities, e.Rarity)),
             ("コーデ", e => e.CoordinationName),
-            ("ジャンル", e => genre.ElementAtOrDefault(e.Genre - 1) ?? e.Genre.ToString()),
-            ("部位", e => category.ElementAtOrDefault(e.Category - 1) ?? e.Category.ToString()),
-            ("ブランド", e => brand.ElementAtOrDefault(e.Brand - 1) ?? e.Brand.ToString()),
-            ("色", e => color.ElementAtOrDefault(e.Color - 1) ?? e.Color.ToString()),
-            ("テイスト", e => subcategory.ElementAtOrDefault(e.SubCategory - 1) ?? e.SubCategory.ToString()),
+            ("ジャンル", e => getValue(genres, e.Genre)),
+            ("部位", e => getValue(categories, e.Category)),
+            ("ブランド", e => getValue(brands, e.Brand)),
+            ("色", e => getValue(colors, e.Color)),
+            ("テイスト", e => getValue(subcategories, e.SubCategory)),
             ("ワッチャ", e => e.Watcha),
             ("画像", e => $"https://cdn.primagi.jp/assets/images/item/{e.Chapter}/{e.Id}.png"),
         };
