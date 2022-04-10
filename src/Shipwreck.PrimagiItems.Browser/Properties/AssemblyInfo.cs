16b609f51f709f1a24b1daf36f156c089d3279b9
pgrho <shipwreck@live.jp>
2022-04-10 23:34:03 +0900

    Added Browser project

---
+++ src/Shipwreck.PrimagiItems.Browser/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+global using System;
+global using System.ComponentModel;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System.Windows;
+global using System.Windows.Controls;
+global using System.Windows.Data;
+global using System.Windows.Documents;
+global using System.Windows.Input;
+global using System.Windows.Media;
+global using System.Windows.Media.Imaging;
+global using System.Windows.Navigation;
+global using System.Windows.Shapes;
+global using Shipwreck.ViewModelUtils;
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
+)]
---
+++ src/Shipwreck.PrimagiItems.Browser/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿namespace Shipwreck.PrimagiItems.Browser.ViewModels;
+
+public class MainWindowViewModel : WindowViewModel
+{
+    #region DocumentTitle
+
+    private string? _DocumentTitle;
+
+    public string? DocumentTitle
+    {
+        get => _DocumentTitle;
+        set => SetProperty(ref _DocumentTitle, value);
+    }
+
+    #endregion DocumentTitle
+
+    #region Location
+
+    private const string LOGIN_URL = "https://primagi.jp/mypage/login/";
+
+    private Uri _Location = new Uri("https://primagi.jp/mypage/");
+    private bool _IsLogin;
+
+    public Uri Location
+    {
+        get => _Location;
+        set
+        {
+            if (SetProperty(ref _Location, value))
+            {
+                IsLogin = _Location?.ToString() == LOGIN_URL;
+            }
+        }
+    }
+
+    public bool IsLogin
+    {
+        get => _IsLogin;
+        private set => SetProperty(ref _IsLogin, value);
+    }
+
+    #endregion Location
+}
