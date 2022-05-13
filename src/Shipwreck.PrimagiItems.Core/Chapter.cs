d9afb8bbab5aefb61c5bd43c236401873da49181
pgrho <shipwreck@live.jp>
2022-05-13 23:04:22 +0900

    Modified IndexPage division

--- src/Shipwreck.PrimagiItems.Core/Chapter.cs
+++ src/Shipwreck.PrimagiItems.Core/Chapter.cs
@@ -6,7 +6,7 @@ public sealed class Chapter : KeyedItem<string>
 {
     public string? DisplayName
         => Key == null ? null
-        : Key == "UNKNOWN" ? "不明"
+        : Key == "UNKNOWN" ? "その他"
         : Regex.Match(Key, "^P0*(\\d+)$") is var m && m.Success ? m.Result("第$1章")
         : Key;
 }
